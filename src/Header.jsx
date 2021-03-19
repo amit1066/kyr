404c44b032b9ad62ffbc6d157b929938cdc0c760
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-03-19 23:02:34 +0530

    added a search bar on the top container

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,48 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import GavelIcon from '@material-ui/icons/Gavel';
-import { makeStyles } from "@material-ui/styles";
+import { fade, makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => ({
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({
   typographyStyles: {
-    flex: 1
+    flex: 1,
+    color:"#fe8500",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  
+  
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   appbarStyle: {
     paddingBottom:"",
     backgroundColor: "#c5cbf5",
-    color:"#fe8500"
+    
   }
 
 }));
@@ -23,7 +55,20 @@ const Header = () => {
         <Typography variant="h3" m="2rem" className={classes.typographyStyles}>
           KNOW YOUR RIGHTS
         </Typography>
-        
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </div>
+        <div className={classes.grow} />
       </Toolbar>
     </AppBar>
   );
