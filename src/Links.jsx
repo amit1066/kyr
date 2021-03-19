9cfd6fbe0b1925d445e182510d4c5dabc58e8f67
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-03-20 00:24:31 +0530

    put Card component into a Box component

--- src/Links.jsx
+++ src/Links.jsx
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Box } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -28,28 +29,31 @@ export default function SimpleCard() {
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        
-        <Typography variant="h5" component="h2">
-          LINKS
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          abcxyz
-        </Typography>
-        <Typography variant="body2" component="p">
-          {bull}link 1 
-          <br />
-          {bull}link 2
-          <br />
-          {bull}link 3
-          <br />
-          {'statement'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Box alignItems="center" className={classes.root}>
+
+      <Card className={classes.root} style={{backgroundColor: "#100a33", color: "white"}}>
+        <CardContent>
+          
+          <Typography variant="h5" component="h2">
+            LINKS
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            abcxyz
+          </Typography>
+          <Typography variant="body2" component="p">
+            {bull}link 1 
+            <br />
+            {bull}link 2
+            <br />
+            {bull}link 3
+            <br />
+            {'statement'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
   );
 }
--- src/Notification.jsx
+++ src/Notification.jsx
@@ -6,10 +6,12 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Box } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    backgroundColor: "#100a33",
   },
   bullet: {
     display: 'inline-block',
@@ -29,27 +31,30 @@ export default function SimpleCard() {
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        
-        <Typography variant="h5" component="h2">
-          NOTIFICATIONS
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          abcxyz
-        </Typography>
-        <Typography variant="body2" component="p">
-          whatever
-          <br />
-          <br/>
-          <br/>
+    <Box pl={20} className={classes.root} alignItems="center" mr={0.5}>
+      
+      <Card style={{backgroundColor: "#100a33", color: "white"}} >
+        <CardContent>
           
-          {'statement'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+          <Typography variant="h5" component="h2">
+            NOTIFICATIONS
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            abcxyz
+          </Typography>
+          <Typography variant="body2" component="p">
+            whatever
+            <br />
+            <br/>
+            <br/>
+            
+            {'statement'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
   );
 }
