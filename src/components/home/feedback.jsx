87eee0fd06e5700affa69661a68c2948c5755dd6
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-04-01 12:30:48 +0530

    decreased the min height of feedback form

--- src/components/home/feedback.jsx
+++ src/components/home/feedback.jsx
@@ -11,10 +11,10 @@ import Button from "@material-ui/core/Button";
 const useStyles = makeStyles((theme) => ({
     AboutUs: {
         backgroundColor: "#ededed",
-    width: "80%",
-    marginLeft: "10%",
-    margin: "50px",
-    padding: "50px",
+        width: "80%",
+        marginLeft: "10%",
+        margin: "50px",
+        padding: "50px",
     },
     SubscribeNow: {
         backgroundColor: "#100a33",
@@ -33,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
         display: 'inline-block',
         maxHeight: "1rem",
         padding: '0px',
-        
+
     }
 }));
 
@@ -43,36 +43,36 @@ const theme = createMuiTheme({
     },
     overrides: {
         MuiButton: {
-          root: {
-            margin: "5px",
-            padding: "2px"
-          }
+            root: {
+                margin: "5px",
+                padding: "2px"
+            }
         }
     }
 });
 // copied this style of text fields from https://material-ui.com/components/text-fields/ react reddit field
-const useStylesReddit = makeStyles((theme) => ({ 
+const useStylesReddit = makeStyles((theme) => ({
     root: {
-      border: '1px solid #e2e2e1',
-      overflow: 'hidden',
-      borderRadius: 5,
-      backgroundColor: '#fcfcfb',
-      transition: theme.transitions.create(['border-color', 'box-shadow']),
-      '&:hover': {
-        backgroundColor: '#fff',
-      },
-      '&$focused': {
-        backgroundColor: '#fff',
-        boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
-        borderColor: theme.palette.primary.main,
-      },
+        border: '1px solid #e2e2e1',
+        overflow: 'hidden',
+        borderRadius: 5,
+        backgroundColor: '#fcfcfb',
+        transition: theme.transitions.create(['border-color', 'box-shadow']),
+        '&:hover': {
+            backgroundColor: '#fff',
+        },
+        '&$focused': {
+            backgroundColor: '#fff',
+            boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+            borderColor: theme.palette.primary.main,
+        },
     },
     focused: {},
 }));
-  
+
 function RedditTextField(props) {
     const classes = useStylesReddit();
-  
+
     return <TextField InputProps={{ classes, disableUnderline: true }} {...props} />;
 }
 
@@ -80,49 +80,50 @@ const Home = () => {
     const classes = useStyles();
     return (
         <div>
-        <Grid item container alignItems="left" justify="center" style={{
-          minHeight: '70vh'}} className={classes.SubscribeNow}>
+            <Grid item container alignItems="left" justify="center" style={{
+                flexGrow: 1,
+            }} className={classes.SubscribeNow}>
                 <Grid item spacing={0} justify="center" alignItems="center">
                     <Typography variant="h4" gutterBottom> Feedback Form</Typography>
                     <form className={classes.root} noValidate>
-              <ThemeProvider theme={theme}>
-                <Grid item container>
-                            <RedditTextField
-                                label="Name"
-                                className={classes.margin}
-                                default-value=""
-                                variant="filled"
-                                id="email"
-                  />
-                </Grid>
-                <Grid item container>
-                            <RedditTextField
-                                label="Email"
-                                className={classes.margin}
-                                default-value=""
-                                variant="filled"
-                                id="email"
-                  />
-                </Grid>
-                <Grid item container>
-                            <RedditTextField
-                                label="Comments"
-                                className={classes.margin}
-                                default-value=""
-                                variant="filled"
-                                id="email"
-                  />
-                </Grid>
-                <Grid item container>
-                            <Button variant="contained" color="primary" size="small" classes={classes.button}>
-                    Send Feedback
-                            </Button>
-                  </Grid>
+                        <ThemeProvider theme={theme}>
+                            <Grid item container>
+                                <RedditTextField
+                                    label="Name"
+                                    className={classes.margin}
+                                    default-value=""
+                                    variant="filled"
+                                    id="email"
+                                />
+                            </Grid>
+                            <Grid item container>
+                                <RedditTextField
+                                    label="Email"
+                                    className={classes.margin}
+                                    default-value=""
+                                    variant="filled"
+                                    id="email"
+                                />
+                            </Grid>
+                            <Grid item container>
+                                <RedditTextField
+                                    label="Comments"
+                                    className={classes.margin}
+                                    default-value=""
+                                    variant="filled"
+                                    id="email"
+                                />
+                            </Grid>
+                            <Grid item container>
+                                <Button variant="contained" color="primary" size="small" classes={classes.button}>
+                                    Send Feedback
+                                </Button>
+                            </Grid>
                         </ThemeProvider>
                     </form>
-                </Grid> 
+                </Grid>
             </Grid>
-           
+
         </div>
     )
 
