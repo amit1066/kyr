fc7e60db90ff0dff25571434d7fbde362c7e5a4b
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-04-04 13:52:23 +0530

    Made the health tab have the same design as the education tab

--- src/components/health.jsx
+++ src/components/health.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+
+import { Grid } from "@material-ui/core";
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -8,6 +10,10 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
@@ -22,7 +28,43 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(2),
     flex: 1,
   },
+  Policy: {
+    backgroundColor: "#CFD5FA",
+    overflow: "visible"
+  },
+  ReadMoreButton: {
+    backgroundColor: "white",
+
+    '&:hover': {
+      backgroundColor: "#100A33",
+      border: "white 0.25rem",
+      color: "white"
+    },
+    'bottom': "-30px",
+  },
+
 }));
+function Policy(props) {
+  const classes = useStyles();
+  return (
+    <Card className={classes.Policy}>
+      <CardContent>
+        <Typography variant="h2" color="textSecondary" style={{ textAlign: "center" }} gutterBottom>
+          {props.title}
+        </Typography>
+        <Typography variant="h5" component="h2" color="textSecondary">
+          {props.subtitle}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {props.short_text}
+        </Typography>
+      </CardContent>
+      <CardActions style={{ justifyContent: "center" }}>
+        <Button size="small" variant="outlined" className={classes.ReadMoreButton}> Read More</Button>
+      </CardActions>
+    </Card>
+  );
+}
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -40,9 +82,7 @@ function HealthDialog(props) {
   }
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={props.handleClickOpen}>
-        {props.title} 
-      </Button>
+      <Policy title="Some policy" subtitle="lorem ipsum" short_text={props.short_text} />
       <Dialog fullScreen open={props.open} onClose={props.handleClose} TransitionComponent={Transition}>
         <AppBar className={classes.appBar}>
           <Toolbar>
@@ -76,9 +116,11 @@ const d1 = {
     "ELIGIBILITY": "  All Craft persons( Handicraft artisans) whether male or female, between the age\
     group of one day to 80 years are eligible to avail the scheme.",
     "BENEFITS OF SCHEME": "  table.",
-    
 
-  }
+
+  },
+  short_text: "Rajiv Gandhi Shilpi Swasthya Bima Yojana is aimed towards providing financial aid to the\
+  artisan community to access the best of healthcare facilities in the country. "
 }
 export default function FullScreenDialog() {
   const [open, setOpen] = React.useState(false);
@@ -93,13 +135,33 @@ export default function FullScreenDialog() {
   };
 
   return (
-    <>
-      <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
-      <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
-      <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
-      <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
-      <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
-      <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
-    </>
+    <div>
+      <Grid item container >
+        <Grid item xs={0} sm={2} />
+        <Grid item xs={5} sm={4} style={{ margin: "20px" }}>
+          <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
+
+        </Grid>
+        <Grid item xs={5} sm={4} style={{ margin: "20px" }} >
+          <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
+        </Grid>
+
+        <Grid item xs={0} sm={2} />
+      </Grid>
+      <Grid item container >
+        <Grid item xs={0} sm={2} />
+        <Grid item xs={5} sm={4} style={{ margin: "20px" }}>
+          <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
+
+        </Grid>
+        <Grid item xs={5} sm={4} style={{ margin: "20px" }} >
+          <HealthDialog open={open === "a"} dialogName="a" handleClose={handleClose} handleClickOpen={() => handleClickOpen("a")} {...d1} />
+
+        </Grid>
+
+        <Grid item xs={0} sm={2} />
+      </Grid>
+
+    </div>
   );
 }
