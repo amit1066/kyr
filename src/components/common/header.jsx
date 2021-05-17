4c1bfba9787e92e2417ce93273cd3c8cc89dd026
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-05-17 09:52:51 +0530

    made the search bar thinner

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -44,7 +44,6 @@ const useStyles = makeStyles((theme) => ({
     }
   },
   inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create('width'),
     width: '85%',
@@ -136,7 +135,7 @@ const Header = () => {
                       <TextField
                         {...params}
                         label="Search"
-                        margin="normal"
+                        
                       />
                     )}
                   />
